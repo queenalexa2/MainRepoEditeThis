049203b2c4a752921b0ecd857722f625520d3d38
cuzierx1 <88854468+cuzierx1@users.noreply.github.com>
2021-08-14 19:21:48 +0530

    Update neotroAlive.js

--- plugins/neotroAlive.js
+++ plugins/neotroAlive.js
@@ -8,6 +8,6 @@ const {MessageType} = require('@adiwajshing/baileys');
 
 Asena.addCommand({pattern: 'workingbot', fromMe: false}, (async (message, match) => {
 
-    await message.sendMessage("*Bang!! Bang!! My Bot is Neotro🎭🙇*");
+    await message.sendMessage("*Bang!! Bang!! My Bot is Cuzier_X🎭🙇*");
 
 }));
